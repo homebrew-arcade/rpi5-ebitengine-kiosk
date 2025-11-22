59f5f7e3ba888c685d49caed6830630eaea52bb1
Justin Horton <bossrighteous@gmail.com>
2025-11-22 13:14:46 -0600

    another systemd attempt from guid

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -18,37 +18,40 @@ git clone https://github.com/homebrew-arcade/rpi5-ebitengine-kiosk.git /home/arc
 # build and run sample app
 echo "Building sample Ebitengine app"
 go build -C /home/arcade/rpi5-ebitengine-kiosk ./cmd/ebitentest/ebitentest.go
-mv /home/arcade/rpi5-ebitengine-kiosk/cmd/ebitentest/ebitentest /home/arcade/
-chmod 755 ebitentest
-echo "Dumping Kiosk ENV vars to /home/arcade/.bashrc"
-echo '' >> /home/arcade/.bashrc
-echo '# Kiosk settings' >> /home/arcade/.bashrc
-echo 'export KIOSK_APP_PATH="/home/arcade/ebitentest"' >> /home/arcade/.bashrc
-echo 'export KIOSK_APP_WATCH=1' >> /home/arcade/.bashrc
-source /home/arcade/.bashrc
+mv /home/arcade/rpi5-ebitengine-kiosk/ebitentest /home/arcade/
+chmod 755 /home/arcade/ebitentest
 echo "Running sample app directly in Cage kiosk"
 echo "Will exit after 15 seconds. Press Q or ESC to exit immediately"
 read -n 1 -s -p "Press any key to run..."
 cage /home/arcade/ebitentest
 
-# configure startup service
-echo "Building Kiosk app"
+# building kiosk process manager
+echo "Building Kiosk Watch app"
 go build -C /home/arcade/rpi5-ebitengine-kiosk/cmd/kiosk kiosk.go
-mv /home/arcade/rpi5-ebitengine-kiosk/cmd/kiosk/kiosk /home/arcade/
-chmod 755 kiosk
-echo "Configuring dumb boot in bashrc with tty1 check"
-echo "# Boot Kiosk on TTY1 start" >> /home/arcade/.bashrc
-echo "if [[ \"$(tty)\" == \"/dev/tty1\"]]; then" >> /home/arcade/.bashrc
-echo "  /home/arcade/kiosk" >> /home/arcade/.bashrc
-echo "fi" >> /home/arcade/.bashrc
+mv /home/arcade/rpi5-ebitengine-kiosk/kiosk /home/arcade/
+chmod 755 /home/arcade/kiosk
+
+# configure startup service
+echo "Configuring kiosk startup service"
+sudo cp /home/arcade/rpi5-ebitengine-kiosk/scripts/cage@.service /etc/systemd/system/cage@.service
+sudo cp /home/arcade/rpi5-ebitengine-kiosk/scripts/pamconf /etc/pam.d/cage
+sudo systemctl daemon-reload
+sudo systemctl enable cage@tty1.service
+sudo systemctl set-default graphical.target
+echo "Re-running sample app from kiosk cage@.service"
+echo "Will exit after 15 seconds. Press Q or ESC to exit immediately"
+read -n 1 -s -p "Press any key to run..."
+sudo systemctl start cage@.service
+sudo systemctl stop cage@.service
 
 # configure auto-login
-echo "Using raspi-config to configure automatic login for arcade user"
-echo "System Options > Boot / Auto Login > B1 Console Autologin"
-read -n 1 -s -p "Press any key to continue..."
-sudo raspi-config
+#echo "Using raspi-config to configure automatic login for arcade user"
+#echo "System Options > Boot / Auto Login > B1 Console Autologin"
+#read -n 1 -s -p "Press any key to continue..."
+#sudo raspi-config
 
 # All done
-echo "All done! Should start boot. Edit .bashrc for ENV updates"
+echo "All done! Should start on boot."
+echo "Edit /etc/systemd/system/cage@.service for kiosk settings"
 read -n 1 -s -p "Press any key to reboot..."
 sudo reboot
